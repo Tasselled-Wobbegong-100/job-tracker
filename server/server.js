35fff207c1059ff158d1a35d32ae775d7d7e0025
Hank Jackson <hjackson277@gmail.com>
2022-05-27 17:38:23 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const apiRouter = require('./routes/api');
+
+const PORT = 3000;
+
+//handle parsing request body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//uses /dist for static files in webpack production mode
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+}
+
+//app entry point
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
+
+
+//handles api endpoints in api routes/api.js
+app.use('/api', apiRouter);
+
+
+
+//unknown route handler
+app.use((req, res) => res.status(404).send('page not found'));
+
+//global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+//starts server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}.`);
+})
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname)
+    },
+    port: 8080, 
+    compress: true,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+};
